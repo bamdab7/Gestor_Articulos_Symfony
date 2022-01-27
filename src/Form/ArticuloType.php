847e68e1d3bdac7013e1d23be62ba0abde300e0a
nekoo <nereapena42gmail.com>
2022-01-27 12:01:10 +0100

    creacion proyecto practica despliegue

---
+++ src/Form/ArticuloType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Articulo;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticuloType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titulo')
+            ->add('fecha')
+            ->add('texto')
+            ->add('comentario')
+            ->add('resumen')
+            ->add('categoria')
+            ->add('url')
+            ->add('medio')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Articulo::class,
+        ]);
+    }
+}
